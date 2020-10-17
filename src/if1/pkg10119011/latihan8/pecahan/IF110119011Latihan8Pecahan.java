41aeef7692c0cbb75d543b2916e082f2f0ed64be
Reihan Wiyanda <Reihan Wiyanda@Mancungs>
2020-10-17 11:42:58 +0700

    IF1-10119011-Latihan8-Pecahan

---
+++ src/if1/pkg10119011/latihan8/pecahan/IF110119011Latihan8Pecahan.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10119011.latihan8.pecahan;
+
+/**
+ *
+ * @author 
+ *  Nama    : Reihan Wiyanda
+ *  Kelas   : IF-1
+ *  Nim     : 10119011
+ */
+public class IF110119011Latihan8Pecahan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        float f;
+        double d;
+        f = 1234567890.123456789F;
+        d = 123_456_789.123456789;
+        System.out.println("Float : " + f);
+        System.out.println("Double : " + d);
+    }
+    
+}
